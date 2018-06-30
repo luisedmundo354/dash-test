ac6a44b7bc3c8dde2cb258f8e1867060da810c11
luisedmundo354 <luisedmundo354@gmail.com>
2018-06-29 21:15:29 -0500

    zsc

--- app.py
+++ app.py
@@ -4,12 +4,18 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import numpy as np
+import pandas as pd
 
 
 app = dash.Dash(__name__)
 server = app.server
 
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+#Read the csv
+df = pd.read_csv('https://s3.us-east-2.amazonaws.com/stats-app-assets/zscores.csv', names=['z','probability'])
+df.apply(pd.to_numeric)
+
 
 app.layout = html.Div([
     html.H2('Hello World'),
@@ -26,19 +32,25 @@ app.layout = html.Div([
 
 
 
-
+#First function
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
 def display_value(value):
     return 'You have selected "{}"'.format(value)
-
-@app.callback(
-    Output(component_id='div-prob', component_property='children'),
-    [Input(component_id='submit-button',component_property='n_clicks')],
-    [State('input-prob','value')])
+#Second function
+@app.callback(dash.dependencies.Output(component_id='div-prob', component_property='children'),
+[dash.dependencies.Input(component_id='submit-button',component_property='n_clicks')],
+[dash.dependencies.State('input-prob','value')])
 def update_output(n_clicks,input):
-    r = np.sin(input)
-    return 'Sin is "{}", tries "{}"'.format(r,n_clicks)
+    ap=10
+    p=0
+    for index,row in df.iterrows():
+        d = abs(row['probability']-input)
+        if d < ap:
+            ap = d
+            p = row['probability']
+    r=df.loc[df['probability']==p,'z']
+    return 'z is "{}", tries "{}"'.format(r.iloc[1] if input>0.5 else r.iloc[0],n_clicks)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
